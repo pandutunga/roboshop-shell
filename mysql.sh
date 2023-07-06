4a1f441ea8838edab14eba8a5dce2c832047f476
Rakesh Kumar Tunga <rt21aah@herts.ac.uk>
2023-07-06 01:00:30 +0100

    mysql sh file

---
+++ mysql.sh
@@ -0,0 +1,15 @@
+echo -e "\e[33m Disable MYSQL Default version\e[0m"
+yum module disable mysql -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Intall MYSQL Community server\e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m start MYSQL service\e[0m"
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl restart mysqld &>>/tmp/roboshop.log
+
+echo -e "\e[33m Setip MYSQL password\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
+
+#mysql -uroot -pRoboShop@1
+
